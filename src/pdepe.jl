d275c6d1403caa0eb179de0b0d8d9dcf75e5afaf
Florens Grimm <florens.grimm@ruhr-uni-bochum.de>
2026-04-01 21:27:46 +0200

    refactor pdepe function: remove unused implicit Euler method implementation and clean up comments

--- src/pdepe.jl
+++ src/pdepe.jl
@@ -61,132 +61,69 @@ function pdepe(m, pdefun::T1, icfun::T2, bdfun::T3, xmesh, tspan; params=SkeelBe
     # Initialize Problem
     Nx, npde, inival, elTv, Ti, pb = problem_init(m, xmesh, tspan, pdefun, icfun, bdfun, params)
 
-    # Solve time dependent problem via Implicit Euler method
-    if params.solver == :euler
-        colors = matrix_colors(pb.jac)::Vector{Ti}
-
-        # Preallocations for Newton's method
-        rhs = zeros(elTv, npde * Nx)
-        cache = ForwardColorJacCache(implicitEuler!, rhs; dx=rhs, colorvec=colors, sparsity=pb.jac)
-
-        # To store the history of the Newton solver
-        if params.hist
-            storage = Vector{elTv}[]
-        end
-
-        # Build the mass matrix
-        mass_mat_diag = mass_matrix(pb)[1]
-        mass_mat_vec = reshape(mass_mat_diag.diag, (npde, Nx))
-
-        # Process Time steps
-        flag_tstep = params.tstep isa Real
-
-        timeSteps, tstep = (flag_tstep ? (collect(tspan[1]:(params.tstep):tspan[2]), params.tstep) :
-                            (params.tstep, diff(params.tstep)))::Tuple{Vector{Float64}, Union{Float64, Vector{Float64}}}
-
-        un = copy(inival)
-        results = pb.npde == 1 ? Vector{elTv}[un] : Matrix{elTv}[reshape(inival, (npde, Nx))]
-
-        for i ∈ eachindex(timeSteps[1:(end - 1)])
-            time_val = timeSteps[i + 1]
-            tstep_val = flag_tstep ? tstep : tstep[i]
-
-            lmul!(mass_mat_diag, un)
-
-            unP1 = newton(un, tstep_val, time_val, pb, mass_mat_vec, cache, rhs; tol=params.tol, maxit=params.maxit,
-                          hist_flag=params.hist, linSol=params.linsolve)
-
-            if params.hist
-                pb.npde == 1 ? push!(results, unP1[1]) :
-                push!(results, reshape(unP1[1], (npde, Nx)))
-                push!(storage, unP1[2])
-                un = unP1[1]
-            else
-                pb.npde == 1 ? push!(results, unP1) : push!(results, reshape(unP1, (npde, Nx)))
-                un = unP1
-            end
-        end
-
-        sol = RecursiveArrayTools.DiffEqArray(results, timeSteps)
-
-        if params.hist && params.data
-            return sol, pb, storage
-        elseif params.hist
-            return sol, storage
-        elseif params.data
-            return sol, pb
-        end
-
-        return sol
-
-        # Use the DifferentialEquations.jl package to solve the system of differential equations 
-    elseif params.solver == :DiffEq
-
-        # returns data from the problem to define an ODEProblem
-        return pb
-    end
+    return pb
 end
 
-"""
-    pdepe(m, pdefunction, icfunction, bdfunction, xmesh ; params=SkeelBerzins.Params(tstep=Inf), kwargs...)
+# """
+#     pdepe(m, pdefunction, icfunction, bdfunction, xmesh ; params=SkeelBerzins.Params(tstep=Inf), kwargs...)
 
-Solve 1D elliptic PDE(s) using the spatial discretization method described in [1] - [`pdepe`](@ref)
-variant to solve stationary problems. Performs one step of the implicit Euler method.
+# Solve 1D elliptic PDE(s) using the spatial discretization method described in [1] - [`pdepe`](@ref)
+# variant to solve stationary problems. Performs one step of the implicit Euler method.
 
-Input arguments:
+# Input arguments:
 
-  - `m`: scalar referring to the symmetry of the problem. It can either take the value `m=0`, `m=1` or
-    `m=2` representing cartesian, cylindrical or spherical coordinates respectively.
-  - `pdefunction`: Function. Defines the formulation of the PDE(s) using capacity, flux and source
-    terms (capacity term should be set to 0).
-  - `icfunction`: Function. It defines the initial value(s) used for the Newton solver.
-  - `bdfunction`: Function. Defines the boundary conditions of the problem.
-  - `xmesh`: 1D array representing the spatial mesh on which the user intends to obtain the solution.
+#   - `m`: scalar referring to the symmetry of the problem. It can either take the value `m=0`, `m=1` or
+#     `m=2` representing cartesian, cylindrical or spherical coordinates respectively.
+#   - `pdefunction`: Function. Defines the formulation of the PDE(s) using capacity, flux and source
+#     terms (capacity term should be set to 0).
+#   - `icfunction`: Function. It defines the initial value(s) used for the Newton solver.
+#   - `bdfunction`: Function. Defines the boundary conditions of the problem.
+#   - `xmesh`: 1D array representing the spatial mesh on which the user intends to obtain the solution.
 
-Keyword argument:
+# Keyword argument:
 
-  - `params`: defines a [`SkeelBerzins.Params`](@ref) structure containing the keyword arguments from
-    the solvers.
-  - `kwargs`: instead of using the [`SkeelBerzins.Params`](@ref) struct, the user can pass directly
-    fields from this particular struct to the solver.
+#   - `params`: defines a [`SkeelBerzins.Params`](@ref) structure containing the keyword arguments from
+#     the solvers.
+#   - `kwargs`: instead of using the [`SkeelBerzins.Params`](@ref) struct, the user can pass directly
+#     fields from this particular struct to the solver.
 
-Returns a 1D Array with the solution available at the points defined by the spatial discretization
-`xmesh`.
-"""
-function pdepe(m, pdefun::T1, icfun::T2, bdfun::T3, xmesh; params=SkeelBerzins.Params(; tstep=Inf), kwargs...) where {T1, T2, T3}
+# Returns a 1D Array with the solution available at the points defined by the spatial discretization
+# `xmesh`.
+# """
+# function pdepe(m, pdefun::T1, icfun::T2, bdfun::T3, xmesh; params=SkeelBerzins.Params(; tstep=Inf), kwargs...) where {T1, T2, T3}
 
-    params = (solver=haskey(kwargs, :solver) ? kwargs[:solver] : params.solver,
-              tstep=haskey(kwargs, :tstep) ? kwargs[:tstep] : Inf,
-              hist=haskey(kwargs, :hist) ? kwargs[:hist] : params.hist,
-              sparsity=haskey(kwargs, :sparsity) ? kwargs[:sparsity] : params.sparsity,
-              linsolve=haskey(kwargs, :linsolve) ? kwargs[:linsolve] : params.linsolve,
-              maxit=haskey(kwargs, :maxit) ? kwargs[:maxit] : params.maxit,
-              tol=haskey(kwargs, :tol) ? kwargs[:tol] : params.tol,
-              data=haskey(kwargs, :data) ? kwargs[:data] : params.data,
-              nb_design_var=haskey(kwargs, :nb_design_var) ? kwargs[:nb_design_var] : params.nb_design_var)
+#     params = (solver=haskey(kwargs, :solver) ? kwargs[:solver] : params.solver,
+#               tstep=haskey(kwargs, :tstep) ? kwargs[:tstep] : Inf,
+#               hist=haskey(kwargs, :hist) ? kwargs[:hist] : params.hist,
+#               sparsity=haskey(kwargs, :sparsity) ? kwargs[:sparsity] : params.sparsity,
+#               linsolve=haskey(kwargs, :linsolve) ? kwargs[:linsolve] : params.linsolve,
+#               maxit=haskey(kwargs, :maxit) ? kwargs[:maxit] : params.maxit,
+#               tol=haskey(kwargs, :tol) ? kwargs[:tol] : params.tol,
+#               data=haskey(kwargs, :data) ? kwargs[:data] : params.data,
+#               nb_design_var=haskey(kwargs, :nb_design_var) ? kwargs[:nb_design_var] : params.nb_design_var)
 
-    @assert params.solver==:euler "Elliptic problems can only be solved using the Euler method"
-    @assert params.tstep==Inf "Time step should be set to Inf to obtain the stationary solution"
+#     @assert params.solver==:euler "Elliptic problems can only be solved using the Euler method"
+#     @assert params.tstep==Inf "Time step should be set to Inf to obtain the stationary solution"
 
-    # Initialize Problem
-    Nx, npde, inival, elTv, Ti, pb = problem_init(m, xmesh, (0, 1), pdefun, icfun, bdfun, params)
+#     # Initialize Problem
+#     Nx, npde, inival, elTv, Ti, pb = problem_init(m, xmesh, (0, 1), pdefun, icfun, bdfun, params)
 
-    colors = matrix_colors(pb.jac)::Vector{Ti}
+#     colors = matrix_colors(pb.jac)::Vector{Ti}
 
-    # Preallocations for Newton's method
-    rhs = zeros(elTv, npde * Nx)
-    cache = ForwardColorJacCache(implicitEuler!, rhs; dx=rhs, colorvec=colors, sparsity=pb.jac)
+#     # Preallocations for Newton's method
+#     rhs = zeros(elTv, npde * Nx)
+#     cache = ForwardColorJacCache(implicitEuler!, rhs; dx=rhs, colorvec=colors, sparsity=pb.jac)
 
-    unP1 = newton_stat(inival, params.tstep, 1, pb, cache, rhs; tol=params.tol, maxit=params.maxit, hist_flag=params.hist,
-                       linSol=params.linsolve)
+#     unP1 = newton_stat(inival, params.tstep, 1, pb, cache, rhs; tol=params.tol, maxit=params.maxit, hist_flag=params.hist,
+#                        linSol=params.linsolve)
 
-    if params.hist && params.data
-        return unP1[1], pb, unP1[2]
-    elseif params.hist
-        return unP1
-    elseif params.data
-        return unP1, pb
-    end
+#     if params.hist && params.data
+#         return unP1[1], pb, unP1[2]
+#     elseif params.hist
+#         return unP1
+#     elseif params.data
+#         return unP1, pb
+#     end
 
-    return unP1
-end
+#     return unP1
+# end
